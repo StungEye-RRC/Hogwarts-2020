db520334059996813ea74f8483e86bc4a094eb91
Kyle Geske <stungeye@gmail.com>
2020-01-30 16:44:38 -0600

    Added validations to the house and student model in anticipation of the seed script.

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,6 @@
 class House < ApplicationRecord
   has_many :students
+
+  validates :name, :points, presence: true
+  validates :points, numericality: { only_integer: true }
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :house # One to many association
+
+  validates :name, presence: true
+  # The FK is also required.
 end
