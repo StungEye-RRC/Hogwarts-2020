85275017c108eb4a81cd3e076a79a74f64cdd44c
Kyle Geske <stungeye@gmail.com>
2020-01-30 17:15:38 -0600

    Added the faker gem and began to create our seed script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Run this script using: rails db:seed
+
+Student.destroy_all
+House.destroy_all
+
+NUMBER_OF_HOUSES = 5
+
+NUMBER_OF_HOUSES.times do
+  house_name = Faker::Movies::HarryPotter.unique.house
+  house_points = rand(333..9999)
+  House.create(name: house_name, points: house_points)
+end
+
+puts "Created #{House.count} Houses."
