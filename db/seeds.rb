978a130fb810cbf1c71a8be6c7ba112e3729259d
Kyle Geske <stungeye@gmail.com>
2020-01-31 16:08:37 -0600

    The seed script is now adding students for each created house.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,14 @@ NUMBER_OF_HOUSES = 5
 NUMBER_OF_HOUSES.times do
   house_name = Faker::Movies::HarryPotter.unique.house
   house_points = rand(333..9999)
-  House.create(name: house_name, points: house_points)
+  house = House.create(name: house_name, points: house_points)
+
+  number_of_students = rand(10..15)
+  number_of_students.times do
+    # The created student's house_id (FK) will be assigned the house's PK.
+    house.students.create(name: Faker::Movies::HarryPotter.unique.character)
+  end
 end
 
 puts "Created #{House.count} Houses."
+puts "Created #{Student.count} Students."
