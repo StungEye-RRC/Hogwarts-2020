71bac76eb398764166688f02f299aaa2e7c5fdaa
Kyle Geske <stungeye@gmail.com>
2020-02-07 16:25:45 -0600

    Added the has many through associations to the student and teacher models.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :house # One to many association
+  has_many :appointments
+  has_many :teachers, through: :appointments
 
   validates :name, presence: true
   # The FK is also required.
